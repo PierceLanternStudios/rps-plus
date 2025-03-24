566f7eaa2b6ee719afe958e8d31fd520a90b0e5c
PierceLanternStudios <pierceclark07@gmail.com>
2025-03-24 11:25:39 -0400

    initialize rps-plus

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+type Move = "rock" | "paper" | "scissors";
+const moves: readonly Move[] = ["rock", "paper", "scissors"];
+
+function GetRandomMove(): Move {
+  return moves[Math.floor(3 * Math.random())];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Computer Move: {GetRandomMove()}</div>;
 }
 
 export default App;
