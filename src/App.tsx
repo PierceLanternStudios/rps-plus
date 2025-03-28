8db4f494f9ee17553a07cc3977d25d6609300d04
PierceLanternStudios <pierceclark07@gmail.com>
2025-03-28 19:38:29 -0400

    removed unnecessary objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { type Move, MOVES } from "./Move";
-import Player from "./Player";
-import AIPlayer from "./AIPlayer";
 import gamePhaseCSS from "./gamePhase.module.css";
 import ResultPhaseCSS from "./resultsPhase.module.css";
 import cardCSS from "./card.module.css";
-import RoundModifier from "./RoundModifier";
-import Card from "./Card";
 
-type GamePhase = "pre-Game" | "Draw" | "Game" | "Results" | "Shop";
+type GamePhase = "pre-Game" | "Game" | "Results";
 type gamePlayer = "You" | "Computer";
 type GameResult = gamePlayer | "Tie";
 
@@ -17,37 +13,18 @@ function App() {
   // =================  Declare State Vars:  ===========================
   // ###################################################################
 
-  const [player, setPlayer] = React.useState<Player>(new Player());
-  const [computer, setComputer] = React.useState<AIPlayer>(new AIPlayer());
+  const [playerMove, setPlayerMove] = React.useState<Move | null>(null);
+  const [computerMove, setComputerMove] = React.useState<Move | null>(null);
   const [gamePhase, setGamePhase] = React.useState<GamePhase>("pre-Game");
   const [round, setRound] = React.useState(0);
-  const [resultsSolved, setResultsSolved] = React.useState<boolean>(false);
-  const [roundPipeline, setRoundPipeline] = React.useState<RoundModifier[]>([]);
-
-  // round-end parameters:
-  const [roundMoney, setRoundMoney] = React.useState<number>(0);
   const [roundResult, setRoundResult] = React.useState<GameResult>("Tie");
-
-  // non-state parameters from round ending:
-  const [roundEffects, setRoundEffects] = React.useState<string[]>([]);
-
-  const [MODIFIER_FUNCTIONS, SMF] = React.useState<RoundModifier[]>(
-    GENERATE_MODIFIER_FUNCTIONS()
-  );
 
   // ###################################################################
   // =================  Callback Functions:  ===========================
   // ###################################################################
   function newRound() {
-    setGamePhase("Draw");
+    setGamePhase("Game");
     setRound(round + 1);
-  }
-
-  function onCardSelected(card: Card) {
-    player.playCard(card);
-    computer.playCard();
-    setResultsSolved(false);
-    setGamePhase("Results");
   }
 
   // ###################################################################
@@ -69,30 +46,24 @@ function App() {
    *                then assigned at the beginning of the first round.
    */
   function processResults() {
-    // check if we've already computed this render:
-    if (resultsSolved) return;
-
-    console.log("Processing Results...");
     // determine who would win this round:
-    if (player.playedCard!.cardType === computer.playedCard!.cardType) {
+    if (playerMove === computerMove) {
+      console.log(playerMove, computerMove);
       setRoundResult("Tie");
     } else {
       setRoundResult(
-        MOVES.indexOf(player.playedCard!.cardType) ===
-          (MOVES.indexOf(computer.playedCard!.cardType) + 1) % 3
+        MOVES.indexOf(playerMove!) === (MOVES.indexOf(computerMove!) + 1) % 3
           ? "Computer"
           : "You"
       );
     }
+    setGamePhase("Results");
+  }
 
-    //perform round-end pipeline effects:
-    for (const mod of roundPipeline) {
-      mod.fn();
-      console.log(roundPipeline.length);
-    }
-
-    // mark this as solved:
-    setResultsSolved(true);
+  function onCardPlayed(cardName: Move) {
+    setPlayerMove("paper");
+    setComputerMove(MOVES[Math.floor(Math.random() * MOVES.length)]);
+    processResults();
   }
 
   // ###################################################################
@@ -104,32 +75,22 @@ function App() {
     return <button onClick={newRound}>Begin Game</button>;
   }
 
-  // render draw phase, in case I want to do any draw animations or anything:
-  function renderDrawPhase() {
-    player.makeHand();
-    computer.makeHand();
-    setGamePhase("Game");
-    return <div>draw!</div>;
-  }
-
   function renderGamePhase() {
     return (
-      <div className={gamePhaseCSS.container}>My Hand: {renderHand()}</div>
+      <div className={gamePhaseCSS.container}>Play a Card! {renderHand()}</div>
     );
   }
 
   function renderResults() {
-    processResults();
+    console.log("rendering!");
     return (
       <div className={ResultPhaseCSS.container}>
         <span>Round #{round}:</span>
-        <span>Computer Played: {computer.playedCard!.DEBUG_toString()}</span>
-        <span>You Played: {player.playedCard!.DEBUG_toString()}</span>
-        <span>Round Effects: {renderRoundEffects()}</span>
+        <span>Computer Played: {computerMove}</span>
+        <span>You Played: {playerMove}</span>
         <span>Round Winner: {roundResult}!</span>
         <span>
           <button onClick={newRound}>New Round!</button>
-          <button onClick={generateShop}>Go To Shop!</button>
         </span>
       </div>
     );
@@ -142,32 +103,18 @@ function App() {
   function renderHand() {
     return (
       <div className={cardCSS.hand_container}>
-        <ul>
-          {player.hand.map((card) => (
-            <button
-              className={cardCSS.button}
-              onClick={() => onCardSelected(card)}
-            >
-              {card.DEBUG_toString()}
-            </button>
-          ))}
-        </ul>
+        <ul>{MOVES.map((move) => renderCard(move))}</ul>
       </div>
     );
   }
 
-  function renderRoundEffects() {
+  function renderCard(cardName: Move) {
     return (
-      <div>
-        <ul>
-          {roundEffects.map((elem) => (
-            <li>{elem}</li>
-          ))}
-        </ul>
-      </div>
+      <button className={cardCSS.button} onClick={() => onCardPlayed(cardName)}>
+        {cardName}
+      </button>
     );
   }
-  function generateShop() {}
 
   // ###################################################################
   // ======================  Main Loop:  ===============================
@@ -175,30 +122,11 @@ function App() {
   //determine what to do based on the phase of the game:
   switch (gamePhase) {
     case "pre-Game":
-      console.log("PREGAME");
       return renderPreGame();
-
-    case "Draw":
-      return renderDrawPhase();
     case "Game":
       return renderGamePhase();
     case "Results":
       return renderResults();
-    case "Shop":
-      return <div>SHOP!</div>;
-  }
-
-  // ###################################################################
-  // ===============  Modifier Function Definitions:  ==================
-  // ###################################################################
-
-  function GENERATE_MODIFIER_FUNCTIONS(): RoundModifier[] {
-    return [
-      new RoundModifier("TestFunction", 100, () => {
-        setRound(100);
-        roundEffects.push("Set round to 100");
-      }),
-    ];
   }
 }
 
